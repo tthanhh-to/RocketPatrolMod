d245631db3a7b4697f5a719b816f85bd8dab69a5
Thanh <thtto@ucsc.edu>
2023-04-20 22:59:06 -0700

    audio is working

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -111,6 +111,7 @@ class Play extends Phaser.Scene {
         });
         // score add and repaint
         this.p1Score += ship.points;
-        this.scoreLeft.text = this.p1Score;       
+        this.scoreLeft.text = this.p1Score;
+        this.sound.play('sfx_explosion');
     }
 }
